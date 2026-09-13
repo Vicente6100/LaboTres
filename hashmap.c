cf5030c033db122fdcfbd221ee4dd3cb9837b8c3
Your Name <you@example.com>
2026-09-13 23:00:24 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -177,7 +177,7 @@ Pair * nextMap(HashMap * map) {
 
 void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
-    (Pair **) old_buckets = map->buckets ;
+    Pair ** old_buckets = map->buckets ;
     long old_capacity = map->capacity ;
     map->capacity *= 2 ;
     map->buckets = (Pair **) calloc (map->capacity, sizeof(Pair *)) ;
